46e2f1e34609cf7bcc0bb10dd9d9b9a1b872509a
JeanPiton <jean.orpiton@gmail.com>
2023-05-12 18:32:02 -0300

    guess data-test

--- src/Chute.js
+++ src/Chute.js
@@ -4,8 +4,8 @@ export default function Chute(props){
     return(
         <CHUTE.areaChute>
             <CHUTE.textoChute>Já sei a palavra!</CHUTE.textoChute>
-            <CHUTE.inputChute disabled={props.enabled} onChange={props.value}/>
-            <CHUTE.buttonChute disabled={props.enabled} onClick={props.func}>Chutar</CHUTE.buttonChute>
+            <CHUTE.inputChute disabled={props.enabled} onChange={props.value} data-test="guess-input"/>
+            <CHUTE.buttonChute disabled={props.enabled} onClick={props.func} data-test="guess-button">Chutar</CHUTE.buttonChute>
         </CHUTE.areaChute>
     );
 }
